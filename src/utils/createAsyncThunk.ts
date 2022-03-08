2d60b6274e5e26803aec87f1c0bee5401cd6df9c
sangminlee98 <tkdals0975@naver.com>
2022-03-08 14:03:23 +0900

    add createAsyncThunk module

---
+++ src/utils/createAsyncThunk.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from 'react';
+import { AsyncActionCreatorBuilder } from 'typesafe-actions';
+
+type AnyCreateAsyncAction = AsyncActionCreatorBuilder<any, any, any>;
+
+export default function createAsyncThunk<A extends AnyCreateAsyncAction, F extends (...params: any[]) => Promise<any>>(
+  asyncActionCreator: A,
+  promiseCreator: F
+) {
+  type Params = Parameters<F>;
+  return function thunk(...params: Params) {
+    return async (dispatch: Dispatch<any>) => {
+      const {request, success, failure} = asyncActionCreator;
+      dispatch(request());
+      try {
+        const response = await promiseCreator(...params);
+        dispatch(success(response));
+      } catch (e) {
+        dispatch(failure(e));
+      }
+    }
+  }
+}
